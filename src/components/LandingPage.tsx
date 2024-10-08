0a2cae1551f5ebb9ceba817d3e010e43eb398359
federico pinho <pinhofederico@gmail.com>
2024-10-08 17:24:06 -0300

    Add follow button to landing

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import LearnModal from './LearnModal';
+import { Twitter } from 'lucide-react';
 
 interface LandingPageProps {
   onPlayClick: () => void;
@@ -16,7 +17,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ onPlayClick }) => {
         className="absolute inset-0 w-full h-full object-cover"
       />
       <div className="relative z-10 flex flex-col items-center justify-center h-full">
-        <div className="flex space-x-4 mt-40">
+        <div className="flex space-x-4 mt-60">
           <button
             onClick={onPlayClick}
             className="bg-green-600 text-white px-6 py-2 rounded text-xl font-bold hover:bg-green-500 transition-colors w-40"
@@ -30,6 +31,15 @@ const LandingPage: React.FC<LandingPageProps> = ({ onPlayClick }) => {
             Learn
           </button>
         </div>
+        <a
+          href="https://twitter.com/ElementalsLab"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-4 bg-blue-400 text-white px-6 py-2 rounded text-xl font-bold hover:bg-blue-500 transition-colors flex items-center"
+        >
+          <Twitter className="mr-2" size={24} />
+          Follow us
+        </a>
       </div>
       <LearnModal isOpen={isLearnModalOpen} onClose={() => setIsLearnModalOpen(false)} />
     </div>
